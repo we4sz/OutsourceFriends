ee148da737bfc5f77424df2540a2df2068183c82
John Johansson <john@johanssonjohn.se>
2015-09-18 21:01:24 +0200

    updated libs and remove AI

---
+++ OutsourceFriends/OutsourceFriends/Context/DomainContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using OutsourceFriends.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OutsourceFriends.Context
+{
+    public class DomainContext: IdentityDbContext<ApplicationUser>
+    {
+
+            public DomainContext()
+                : base("DefaultConnection", throwIfV1Schema: false)
+            {
+            }
+
+            public static DomainContext Create()
+            {
+                return new DomainContext();
+            }
+        }
+    }
+}
